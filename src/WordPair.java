01a14680bfb1d5d33529dd0a4542e875a1963054
J <jaylauhc@gmail.com>
2024-10-06 17:17:00 +0800

    Complete

---
+++ src/WordPair.java
@@ -0,0 +1,21 @@
+public class WordPair {
+    String word1,word2;
+
+    public WordPair(String a,String b){
+        word1 = a;
+        word2 = b;
+    }
+
+    public String getFirst()
+    {
+        return word1;
+
+    }
+
+    public String getSecond()
+    {
+
+        return word2;
+
+    }
+}
